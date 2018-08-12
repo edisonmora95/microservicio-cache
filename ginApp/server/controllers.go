d7b72b89c0e288ad28622b8c5a958af8d854f68a
Jose <josevns95@gmail.com>
2018-08-12 04:44:48 -0500

    api endpoint to retrieve top 10 gifs

--- ginApp/server/controllers.go
+++ ginApp/server/controllers.go
@@ -21,3 +21,34 @@ func showIndexPage(c *gin.Context){
 		},
 	)	
 }
+
+
+func tenGifsJson (c *gin.Context){
+
+	file := retrieve_all_gifs("gifs/")
+	// Call the HTML method of the Context to render a template
+	c.JSON(
+		http.StatusOK,
+		gin.H{
+		"counter": 3,
+		"file": file,
+		"timestamp": 35,
+		},
+	)	
+}
+
+
+
+func tenGifsMysqlJson (c *gin.Context){
+	// Call the HTML method of the Context to render a template
+	c.HTML(
+		// Set the HTTP status to 200 (OK)
+		http.StatusOK,
+		// Use the index.html template
+		"index.html",
+		// Pass the data that the page uses (in this case, 'title')
+		gin.H{
+		"title": "Home Page",
+		},
+	)	
+}
--- ginApp/server/routes.go
+++ ginApp/server/routes.go
@@ -6,4 +6,11 @@ func initializeRoutes() {
 
   // Handle the index route
   router.GET("/", showIndexPage)
+
+  //Handle json response of 10 queries
+  router.GET("/api/tenGifs", tenGifsJson)
+
+  //Handle json response of 10 queries (mysql)
+  router.GET("/api/tenGifsMysql", tenGifsMysqlJson)
+  
 }
